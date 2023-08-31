53bcea3483e6def00069c4057eddd31702f2c350
Dufig <vojtech.fila@mensa.cz>
2023-08-31 15:33:40 +0200

    Some face and point work

--- main.py
+++ main.py
@@ -3,18 +3,92 @@ import openpyscad as ops
 f = open("kvadr.scad", "r")
 f = f.read()
 
-def find_list(no_list: str) -> list:
-    no_list = no_list[:no_list.index(";")]
-    no_list = no_list[no_list.index("["):]
-    return list(no_list)
+
+def find_points(no_list: str) -> list:
+    points = []
+    no_list = no_list[:no_list.index(";") - 1]
+    no_list = no_list[no_list.index("[") + 1:]
+    no_list = no_list + ",L"
+    coor = []
+    for i in range(no_list.count("[")):
+        for j in range(3):
+            temp = no_list[:no_list.index(",")]
+            no_list = no_list[no_list.index(",") + 1:]
+            if j == 0:
+                temp = temp[temp.find("[") + 1:]
+            if j == 2:
+                temp = temp[:-1]
+            temp = int(temp)
+            coor.append(temp)
+            if j == 2:
+                points.append(coor)
+                coor = []
+
+    return points
 
 
-Points = list(find_list(f))
-print(type(Points))
+def find_faces(no_list: str) -> list:
+    faces = []
+    no_list = no_list[no_list.index("Faces"):]
+    no_list = no_list[no_list.index("[") + 1:no_list.index(";") - 1]
+    no_list = no_list + ",L"
+    coor = []
+    number = int(no_list.count(",")/no_list.count("["))
+    for i in range(no_list.count("[")):
+        for j in range(number):
+            temp = no_list[:no_list.index(",")]
+            no_list = no_list[no_list.index(",") + 1:]
+            if j == 0:
+                temp = temp[temp.find("[") + 1:]
+            if j == number - 1:
+                temp = temp[:-1]
+            temp = int(temp)
+            coor.append(temp)
+            if j == number - 1:
+                faces.append(coor)
+                coor = []
+
+    return faces
 
 
+def not_top_faces(points: list, faces: list) -> list:
+    heights = []
+    bad_points = []
+    good_faces = []
+    for point in points:
+        if point[2] not in heights:
+            heights.append(point[2])
+    top = max(heights)
+    for i in range(len(points)):
+        if not(points[i][2] >= top):
+            bad_points.append(points[i])
+    for face in faces:
+        for point in face:
+            if points[point] in bad_points:
+                good_faces.append(face)
+                break
 
+    return good_faces
+
+
+def worthless_points(points: list) -> list:
+    heights = []
+    bad_points = []
+    for point in points:
+        if point[2] not in heights:
+            heights.append(point[2])
+    top = max(heights)
+    for i in range(len(points)):
+        if not(points[i][2] >= top):
+            bad_points.append(points[i])
+    for point in bad_points:
+        points.remove(point)
+    return bad_points
 
 
 if __name__ == '__main__':
-    pass
+    Points = list(find_points(f))
+    Faces = list(find_faces(f))
+    Faces = list(not_top_faces(Points, Faces))
+    print(Faces)
+    worthless_points(Points)
